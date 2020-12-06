28ded2ae32d9d4872aad79f61cff706c97550517
christopher <christopher.zelezny@mailbox.org>
2020-12-06 16:49:44 +0100

    check century years

--- src/main/java/LeapYear.java
+++ src/main/java/LeapYear.java
@@ -1,2 +1,18 @@
 public class LeapYear {
+    public boolean checkLeapYear(int year) {
+        if(isCenturyYear(year)) {
+            if(year % 400 == 0) {
+                return true;
+            } else return false;
+        }
+        if(year % 4 == 0) {
+            return true;
+        } else return false;
+    }
+
+    boolean isCenturyYear(int year) {
+        if(year % 4 == 0 && year % 100 == 0) {
+            return true;
+        } else return false;
+    }
 }
--- src/test/java/LeapYearTest.java
+++ src/test/java/LeapYearTest.java
@@ -14,8 +14,28 @@ class LeapYearTest {
     }
 
     @Test
-    @DisplayName("test if test environment work correct")
-    void testIfTestEnvironmentWorkCorrect() {
-        assertEquals(1,2 );
+    @DisplayName("test return value from new method")
+    void testReturnValueFromNewMethod() {
+        assertEquals(true, leapYear.checkLeapYear(2000));
     }
+
+    @Test
+    @DisplayName("test year 1999 return false")
+    void testYear1999ReturnFalse() {
+        assertEquals(false,leapYear.checkLeapYear(1999) );
+    }
+
+
+    @Test
+    @DisplayName("check if century year ends with 00 is a leap year or not")
+    void checkIfCenturyYearEndsWith00IsALeapYearOrNot() {
+        assertEquals(true ,leapYear.checkLeapYear(400) );
+    }
+
+    @Test
+    @DisplayName("check the century years that are not leap years")
+    void checkTheCenturyYearsThatAreNotLeapYears() {
+        assertEquals(false,leapYear.checkLeapYear(100) );
+    }
+
 }
